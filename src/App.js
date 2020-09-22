2f4c8adf2029a78e24f6715f29daf5610d76cd78
aliensmart <kaoua17@gmail.com>
2020-09-22 05:51:48 -0400

    projectSummary created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import {BrowserRouter} from 'react-router-dom'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
+import Dashboard from './components/dashboard/Dashboard'
+
 
 
 
@@ -9,6 +11,9 @@ function App() {
     <BrowserRouter>
       <div className="App">
         <Navbar/>
+        <Switch>
+          <Route path='/' component={Dashboard}/>
+        </Switch>
       </div>
     </BrowserRouter>
   );
